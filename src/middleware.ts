0ae6e66da76e1cca40f15d4af66683d3b159f20f
Simon Kågedal Reimer <skagedal@gmail.com>
2025-06-01 12:53:10 +0200

    allow explicit any in this hack

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,6 +5,7 @@ export const config = {
   runtime: "nodejs",
 };
 
+/* eslint-disable @typescript-eslint/no-explicit-any */
 function createProxy<T extends (...args: any[]) => any>(targetFunction: T): T {
   return ((...args: Parameters<T>) => {
     if (process.env.MIDDLEWARE === "true") {
@@ -16,5 +17,6 @@ function createProxy<T extends (...args: any[]) => any>(targetFunction: T): T {
     }
   }) as T;
 }
+/* eslint-enable @typescript-eslint/no-explicit-any */
 
 export const middleware = createProxy(authMiddleware);
