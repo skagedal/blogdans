7b1450e299c5c37143d62fbfd28f21e433782145
Simon Kågedal Reimer <skagedal@gmail.com>
2025-06-01 12:50:32 +0200

    another attempt at a logging middleware

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
   output: "standalone",
+  experimental: {
+    nodeMiddleware: true,
+  }
 };
 
 export default nextConfig;
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,1 +1,20 @@
-export { auth as middleware } from "@/auth"
+import { auth as authMiddleware } from "@/auth";
+import { logger } from "./logger";
+
+export const config = {
+  runtime: "nodejs",
+};
+
+function createProxy<T extends (...args: any[]) => any>(targetFunction: T): T {
+  return ((...args: Parameters<T>) => {
+    if (process.env.MIDDLEWARE === "true") {
+      const result = targetFunction(...args);
+      logger.info("Invoked auth middleware");
+      return result;
+    } else {
+      console.log('Skipping auth middleware invocation');
+    }
+  }) as T;
+}
+
+export const middleware = createProxy(authMiddleware);
