77f249353adf23928151007936821c991fb37247
Simon Kågedal Reimer <skagedal@gmail.com>
2025-06-01 12:57:06 +0200

    one more attempt

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,22 +1,7 @@
 import { auth as authMiddleware } from "@/auth";
-import { logger } from "./logger";
 
 export const config = {
   runtime: "nodejs",
 };
 
-/* eslint-disable @typescript-eslint/no-explicit-any */
-function createProxy<T extends (...args: any[]) => any>(targetFunction: T): T {
-  return ((...args: Parameters<T>) => {
-    if (process.env.MIDDLEWARE === "true") {
-      const result = targetFunction(...args);
-      logger.info("Invoked auth middleware");
-      return result;
-    } else {
-      console.log('Skipping auth middleware invocation');
-    }
-  }) as T;
-}
-/* eslint-enable @typescript-eslint/no-explicit-any */
-
-export const middleware = createProxy(authMiddleware);
+export const middleware = authMiddleware;
