5036aff12c3abe83eb95058becd51a91dd7fe60d
Simon Kågedal Reimer <skagedal@gmail.com>
2025-06-01 12:59:57 +0200

    avoid the file logs

--- src/logger.ts
+++ src/logger.ts
@@ -3,6 +3,6 @@ import winston from 'winston';
 export const logger = winston.createLogger({
     transports: [
         new winston.transports.Console(),
-        new winston.transports.File({ filename: 'logs/combined.log' })
+        // new winston.transports.File({ filename: 'logs/combined.log' })
     ]
 });
