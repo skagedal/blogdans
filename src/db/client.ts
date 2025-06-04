57a30c48e7f7342068b50c03dd135f4502a8af6b
Simon Kågedal Reimer <skagedal@gmail.com>
2025-06-04 06:41:23 +0200

    give right database credentials

--- src/db/client.ts
+++ src/db/client.ts
@@ -1,9 +1,18 @@
 import { Kysely, PostgresDialect } from "kysely";
+import fs from 'fs';
 import { Pool } from "pg";
 import type { DB } from "./schema";
 
 export const db = new Kysely<DB>({
   dialect: new PostgresDialect({
-    pool: new Pool({ connectionString: process.env.DATABASE_URL })
+    pool: new Pool({ connectionString: (() => {
+      const passwordFile = process.env.DATABASE_PASSWORD_FILE || '/run/secrets/db-password';
+      const password = fs.readFileSync(passwordFile, 'utf8').trim();
+      const user = process.env.DATABASE_USER as string;
+      const host = process.env.DATABASE_HOST as string;
+      const database = process.env.DATABASE_NAME as string;
+      const sslmode = process.env.DATABASE_SSLMODE || 'disable';
+      return `postgresql://${user}:${password}@${host}:5432/${database}?sslmode=${sslmode}`;
+    })() })
   })
 });
