8c6b9fdf63ebe56c1ddeedd0ea193754e51e230c
Simon Kågedal Reimer <skagedal@gmail.com>
2025-07-12 16:16:37 +0200

    add logging

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,4 +1,7 @@
+import { logger } from '@/logger';
+
 export async function register() {
+  logger.info('Registering instrumentation...', { runtime: process.env.NEXT_RUNTIME });
   if (process.env.NEXT_RUNTIME === 'nodejs') {
     const { syncPostsToDatabase } = await import('./src/lib/sync-posts');
     await syncPostsToDatabase();
