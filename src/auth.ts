1fbee64257bceea5364b83934e639a882270459b
Simon Kågedal Reimer <skagedal@gmail.com>
2025-07-12 06:47:04 +0200

    Revert "add some token handling and logging"
    This reverts commit 315bb17cf7d68a2f71076a10f1c3b533bc76769f.

--- src/auth.ts
+++ src/auth.ts
@@ -3,46 +3,15 @@ import Google from "next-auth/providers/google"
 import { logger } from "./logger";
 import { createUser } from "./db/repository"
 import { googleProfileSchema } from "./lib/google-types";
-import { Service } from "./lib/service";
 
 const isMockMode = process.env.MOCK_USER === "true";
-
-const service = new Service();
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: isMockMode ? [] : [Google],
   callbacks: isMockMode ? {} : {
     jwt: async ({ token, user, account, profile }) => {
-      if (!profile) {
-        logger.info("JWT callback triggered without profile, just return token", { token });
-        return token;
-      }
-
-      try {
-        logger.info("JWT callback triggered with profile", { token, user, account, profile });
-        const googleProfile = googleProfileSchema.parse(profile);
-        const ourUser = await service.getBlogUserFromGoogleId(googleProfile.sub);
-        if (ourUser.$case === "authenticated") {
-          return {
-            ...token,
-            blogdansUser: ourUser,
-            googleId: googleProfile.sub,
-          }
-        } else {
-          throw new Error("User not authenticated");
-        }
-      } catch (error) {
-        logger.error("Error parsing Google profile", { error });
-        throw error;
-      }
-    },
-    session: async ({ session, token }) => {
-      logger.info('Session callback triggered', { session, token });
-      return {
-        ...session,
-        blogdansUser: token.blogdansUser,
-        googleId: token.googleId,
-      }
+      logger.info("JWT callback triggered", { token, user, account, profile });
+      return token
     },
     signIn: async ({ user, account, profile }) => {
       logger.info("SignIn callback triggered", { user, account, profile });
