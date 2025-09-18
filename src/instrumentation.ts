2f5fb436f18c79715a08ee60bb1182af484089ed
Simon Kågedal Reimer <skagedal@gmail.com>
2025-09-18 19:03:44 +0200

    Add back db sync code, but under try/catch

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -2,8 +2,12 @@ export async function register() {
   const { logger } = await import('@/logger');
   logger.info('Registering instrumentation...', { runtime: process.env.NEXT_RUNTIME });
   if (process.env.NEXT_RUNTIME === 'nodejs') {
-    // const { syncPostsToDatabase } = await import('@/lib/sync-posts');
-    // await syncPostsToDatabase();
-    logger.info('Database stuff is not enabled at the moment.');
+    try {
+      const { syncPostsToDatabase } = await import('@/lib/sync-posts');
+      await syncPostsToDatabase();
+      logger.info('Post synchronization completed successfully');
+    } catch (error) {
+      logger.error('Error occurred while syncing posts to database', error);
+    }
   }
 }
