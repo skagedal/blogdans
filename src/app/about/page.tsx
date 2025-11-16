987d93912d7df7a5c490b07df985e999d7ed2863
Simon Kågedal Reimer <skagedal@gmail.com>
2025-11-16 13:49:09 +0100

    show number of posts on about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,15 +1,25 @@
+import { db } from "@/db/client";
+import { sql } from "kysely";
 import { markdownComponents } from "@/components/markdown-components";
 import Markdown from "react-markdown";
+import z from "zod";
+
+const countSchema = z.object({
+  count: z.string(),
+});
 
 export default async function AboutPage() {
+  const result = await db.executeQuery(
+    sql`SELECT COUNT(*) AS count FROM post`.compile(db)
+  );
+  const row = countSchema.parse(result.rows[0]);
+  
   return (
     <article>
       <Markdown components={markdownComponents}>
         {`
 # Markdown Example
-This is a simple markdown example.
-## Subheading
-Here is some code:
+There are currently **${row.count}** posts in the database.
 `}
       </Markdown>
     </article>
--- src/lib/trpc/routers/test.ts
+++ src/lib/trpc/routers/test.ts
@@ -1,11 +1,16 @@
 import { db } from "@/db/client";
 import { publicProcedure, router } from "../server";
 import { sql } from "kysely";
+import z from "zod";
+
+const countSchema = z.object({
+  count: z.string(),
+});
 
 export const testRouter = router({
   numberOfPosts: publicProcedure.query(async ({ }) => {
     const result = await db.executeQuery(sql`SELECT COUNT(*) AS count FROM post`.compile(db));
-    const row = result.rows[0]
-    return { count: JSON.stringify(row) };
+    const row = countSchema.parse(result.rows[0]);
+    return { count: row.count };
   }),
 });
