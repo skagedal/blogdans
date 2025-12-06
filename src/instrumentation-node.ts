41c11e96d91cf40b8e681b4e6b8aedbdd1b17fc6
Simon Kågedal Reimer <skagedal@gmail.com>
2025-12-06 14:58:02 +0100

    cleanup because weird edge runtime errors in instrumentation

---
+++ src/instrumentation-node.ts
@@ -0,0 +1,16 @@
+import { logger } from "@/logger";
+import { syncPostsToDatabase } from "@/lib/sync-posts";
+
+export async function serverStartup() {
+  logger.info("Starting service", { runtime: process.env.NEXT_RUNTIME });
+  try {
+    logger.info("Starting synchronization of posts to database");
+    const { successCount, failureCount } = await syncPostsToDatabase();
+    if (failureCount > 0) {
+      logger.warn(`Failed to synchronize ${failureCount} posts to database`);
+    }
+    logger.info(`Successfully synchronized ${successCount} posts to database`);
+  } catch (error) {
+    logger.error("Error occurred while syncing posts to database", error);
+  }
+}
--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,22 +1,6 @@
-async function serverStartup() {
-  const { logger } = await import("@/logger");
-  logger.info("Starting service", { runtime: process.env.NEXT_RUNTIME });
-  try {
-    const { syncPostsToDatabase } = await import("@/lib/sync-posts");
-
-    logger.info("Starting synchronization of posts to database");
-    const { successCount, failureCount } = await syncPostsToDatabase();
-    if (failureCount > 0) {
-      logger.warn(`Failed to synchronize ${failureCount} posts to database`);
-    }
-    logger.info(`Successfully synchronized ${successCount} posts to database`);
-  } catch (error) {
-    logger.error("Error occurred while syncing posts to database", error);
-  }
-}
-
 export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
+    const { serverStartup } = await import("./instrumentation-node");
     await serverStartup();
   }
 }
